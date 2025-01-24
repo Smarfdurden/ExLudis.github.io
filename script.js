7290c354786f6841756fe4db5233277623feedb3
Smarfdurden <100794294+Smarfdurden@users.noreply.github.com>
2025-01-24 13:29:34 -0500

    optimize images

--- script.js
+++ script.js
@@ -38,23 +38,4 @@ toggleButton.addEventListener('click', () => {
   updateDarkModeUI(isDarkMode);
 });
 
-// Select the button
-const scrollToTopButton = document.getElementById("scrollToTop");
-
-// Add an event listener for scrolling
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 300) { // Show button after scrolling down 300px
-    scrollToTopButton.classList.add("show");
-  } else {
-    scrollToTopButton.classList.remove("show");
-  }
-});
-
-// Scroll to top when the button is clicked
-scrollToTopButton.addEventListener("click", () => {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth" // Smooth scrolling
-  });
-});
 
